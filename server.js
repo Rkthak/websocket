778587581a89accbfb9d51c4c308f079d48a0915
Rkthak <rkon2609@gmail.com>
2026-07-13 13:28:33 +0530

    express, http, webSocket server + Browser page

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require("express");
+const http = require("http");
+const webSocket = require("ws");
+const { PORT } = require("./utils/config");
 
+// express server
 const app = express();
+
+app.use(express.static("public"));
+
+// http server
+const server = http.createServer(app);
+
+// webSocket server
+const wss = new webSocket.Server({ server });
+
+server.listen(PORT, () => {
+  console.log("Server running...");
+});
